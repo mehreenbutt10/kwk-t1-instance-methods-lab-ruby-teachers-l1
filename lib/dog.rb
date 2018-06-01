c8d33ac4898dcde7638a1505b18acd583e7c248c
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-01 19:39:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,10 @@
 class Dog 
+  def bark
+    puts "Woof!"
+  end
+
+  def sit 
+    puts "The Dog is sitting"
+  end
+
 end 
-
-def bark
-  puts "Woof!"
-end
-
-def sit 
-  puts "The Dog is sitting"
-end
-
-fido = Dog.new 
-fido.bark 
-fido.sit
